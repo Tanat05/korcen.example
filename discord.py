f9a9ba29584c5979bbdc7178445ce838e1d69576
이루 <100296449+erukim@users.noreply.github.com>
2022-08-11 09:34:55 +0900

    Create discord.py

---
+++ discord.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commads
+from korcen import korcen
+
+korcen = korcen.korcen()
+
+@bot.event
+async def on_message(message):
+    print(korcen.check(message.content))
+    if korcen.check(message.content):
+        await message.delete()
+        embed = nextcord.Embed(title = "메세지 삭제함", description = "", color=0xFF2424)
+        embed.set_footer(text = "© Korcen")
+        await message.channel.send(embed=embed)
